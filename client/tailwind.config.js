ecc616d3743e9e8047917564a9a1cb9dd3793c59
caglapekoglu <cagla.pekoglu01@gmail.com>
2022-08-12 17:13:12 +0300

    .

---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,tsx,ts,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'purple':'#6320EE'
+      }
+    },
+  },
+  plugins: [Myclass],
+}
