01db9fa4cb46a9a981426d8fad9b862b0bfa39a1
caglapekoglu <cagla.pekoglu01@gmail.com>
2022-08-16 11:24:30 +0300

    img
    img

--- client/src/screens/MoodScreen.jsx
+++ client/src/screens/MoodScreen.jsx
@@ -5,12 +5,12 @@ function MoodScreen() {
   return (
     <div className='w-full h-full flex flex-col justify-center'>
         <div className="w-full h-full z-100">
-            <img className="w-full h-[3000px] absolute" src={bgImg} alt="" />
-            <div className='w-full h-[3000px] absolute bg-purple/70 '></div>
+            <img className="w-full h-[2000px] absolute" src={bgImg} alt="" />
+            <div className='w-full h-[2000px] absolute bg-purple/70 '></div>
         </div>
-        <div className='max-w[1240px] m-auto px-20 bg-content bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl backdrop-filter-blur'>
+        <div className='max-w[1240px] my-10 mx-40 px-20 bg-content bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl backdrop-filter-blur'>
           <h1 className='text-3xl text-white py-5 text-center font-bold'>Moods</h1>
-            <div className="grid grid-cols-2 lg:grid-cols-4 relative gap-x-8 gap-y-5 gay-y-16 px-4 text-black">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 relative gap-x-8 gap-y-5 gay-y-16 p-4 text-black">
                 <MoodCard title='Deniz Kenarı' content="Kendinizi deniz kenarında hissetmeye hazır mısınız?" bgImg={bgImg}></MoodCard>
                 <MoodCard></MoodCard>
                 <MoodCard></MoodCard>
