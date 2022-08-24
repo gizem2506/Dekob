389628b375fdf36d571af564059382e6c4ebe61b
Dogus Gunes <dogusgunes68@gmail.com>
2022-08-24 22:04:24 +0300

    socket configured

--- server/controller/mood.js
+++ server/controller/mood.js
@@ -126,16 +126,6 @@ exports.getAllMoods = async (req, res) => {
   }
 };
 
-function base64_decode(base64str, file) {
-  // create buffer object from base64 encoded string,
-  // it is important to tell the constructor
-  // that the string is base64 encoded
-  var bitmap = Buffer.from(base64str, "base64");
-  // write buffer to file
-  fs.writeFileSync(file, bitmap);
-  console.log("**** File created from base64 encoded string *****");
-}
-
 exports.addFileToDB = async (req, res) => {
   try {
     /*
