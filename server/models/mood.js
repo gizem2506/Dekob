6ba29471382ac2f885927ea65d40ca4c2fd1a5f7
Dogus Gunes <dogusgunes68@gmail.com>
2022-08-24 11:40:46 +0300

    category filter

--- server/models/mood.js
+++ server/models/mood.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { Blob } = require("buffer");
 const moodSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -9,9 +8,7 @@ const moodSchema = new mongoose.Schema({
     type: String,
     required: [true, "A mood must have a category"],
   },
-  conent: {
-    type: String,
-  },
+  content: String,
   img: [
     {
       data: Buffer,
@@ -21,5 +18,4 @@ const moodSchema = new mongoose.Schema({
 });
 
 const Mood = mongoose.model("Mood", moodSchema);
-
 module.exports = Mood;
