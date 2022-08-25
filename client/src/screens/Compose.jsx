1aadb49abf66387d57536ba40bf0133e83340293
caglapekoglu <cagla.pekoglu01@gmail.com>
2022-08-25 22:52:22 +0300

    add refresh

--- client/src/screens/Compose.jsx
+++ client/src/screens/Compose.jsx
@@ -5,14 +5,18 @@ import { io } from "socket.io-client";
 import { useState } from "react";
 let socket;
 function Chatting() {
-  const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState(JSON.parse(localStorage?.getItem('messageList')) || []);
   const [userName, setUserName] = useState(undefined);
   const [isUserNameCreated, setUserNameCreated] = useState(false);
+  const onFormSubmit = e => {
+    e.preventDefault();
+    // send state to server with e.g. `window.fetch`
+  }
   React.useEffect(() => {
-
+    console.log(messageList);
     if(!socket){
-    socket = io("ws://192.168.1.70:5002",{
+    socket = io("ws://192.168.1.5:5002",{
       'reconnection': true,
       'reconnectionDelay': 500,
       'reconnectionAttempts': 10
@@ -26,15 +30,22 @@ function Chatting() {
         console.log("new event", data);
         messageList.push(data);
         setMessageList([...messageList])
+        localStorage.setItem("messageList", JSON.stringify(messageList));
       });
       
       socket.on("disconnect", () => {
         console.log("onDisconnect"); 
       });
     }
-
   }, []);
   
+  console.log(JSON.parse(localStorage.getItem('messageList')))
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+    
+  }
+
   return (
     <div>
       { !isUserNameCreated ?
@@ -44,6 +55,7 @@ function Chatting() {
             <div className='w-full h-screen absolute  bg-purple/70 '></div>
         </div>
         <div className='max-w[1240px] m-auto px-10 md:px-40 text-white z-10'>
+          <form onSubmit={onFormSubmit}>
             <div className='flex flex-col md:flex-row md:items-center'>
                 <p className='mr-3'></p>
                 <h1 className='text-xl font-bold'>Kullanıcı Adı: </h1>
@@ -53,7 +65,8 @@ function Chatting() {
                 setUserName(e.target.value)
               }} />
             </div>
-            <button onClick={()=>setUserNameCreated(true)} className='text-purple px-[16px] py-[8px] bg-white flex float-right items-center rounded-lg'><span className='hidden md:block'>Devam et</span><ArrowRightIcon className='w-5 md:ml-2'></ArrowRightIcon></button>
+            <button type='submit' onClick={()=>setUserNameCreated(true)} className='text-purple px-[16px] py-[8px] bg-white flex float-right items-center rounded-lg'><span className='hidden md:block'>Devam et</span><ArrowRightIcon className='w-5 md:ml-2'></ArrowRightIcon></button>
+            </form>
         </div>
     </div>):
 
@@ -64,23 +77,24 @@ function Chatting() {
             <div className='w-full h-screen absolute  bg-purple/70 '></div>
         </div>
         <div className='max-w[1240px] h-[70%] m-auto px-10 md:px-40 text-white z-10 '>
+          <form onSubmit={onFormSubmit} >
             <div className='flex flex-col md:flex-row md:items-center'>
                 <p className='mr-3'></p>
                 <h1 className='text-xl font-bold'>Chat! </h1>
             </div>
             <div className='overflow-scroll scroll-y-auto h-[300px]'>
-            {messageList.slice(messageList.length-12 < 0 ? 0 : messageList.length-12 ,messageList.length).map((message)=>{
-                return (<div><p>{message.userName}:{message.message} {message.time}</p></div>)
+            {messageList?.slice(messageList.length-12 < 0 ? 0 : messageList.length-12 ,messageList.length).map((message)=>{
+                return (<div className='flex justify-between'><p>{message.userName}:{message.message}</p><p>{message.time}</p></div>)
               })}
             </div>
             <div className='flex flex-col'>
-              <input className='w-[500px] mb-7 h-[40px] text-black' type="text" onChange={(e)=>{
-                setMessage(e.target.value)
-              }} />
+              <input className='w-[500px] mb-7 h-[40px] text-black' type="text" onChange={(e)=>handleMessage(e)} /> 
+              {/* setMessage(e.target.value) */}
             </div>
-            <button onClick={()=> {
+            <button type="submit" onClick={()=> {
               socket.emit("event",{message: message, userName:userName})
             }} className='text-purple px-[16px] py-[8px] bg-white flex float-right items-center rounded-lg'><span className='hidden md:block'>Gönder</span><ArrowRightIcon className='w-5 md:ml-2'></ArrowRightIcon></button>
+            </form>
         </div>
     </div>)}
 </div>
